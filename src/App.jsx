3a0fa4083f899714035417ae7cf6602364831d55
Vamika Singh <vamika19998singh@gmail.com>
2026-01-19 15:11:54 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import ToDoList from "./components/ToDoList";
-import "./styles.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
-  return(
+  return (
     <div className="container">
       <Header />
       <ToDoList todos={todos} setTodos={setTodos} />
